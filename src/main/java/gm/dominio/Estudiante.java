d1eea02da807df5733c03d627173d78429a974e6
Rodolfo <rodolfo.parada.gonzalez@gmail.com>
2024-08-06 22:11:25 -0400

    se conecto a la base de datos local e interactuo con el patron de datos DAO, se crearón metodos, solo se debe corregir el metodo agregar Estudiante no se refleja los datos en la base de datos.

---
+++ src/main/java/gm/dominio/Estudiante.java
@@ -0,0 +1,73 @@
+package gm.dominio;
+
+public class Estudiante {
+    private int idEstudiante;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+
+    public Estudiante(String carlos, String lara, String number, String mail){}
+
+    public Estudiante(int idEstudiante){
+        this.idEstudiante = idEstudiante;
+    }
+    public Estudiante(int idEstudiante, String nombre, String apellido, String telefono, String email){
+        this.idEstudiante = idEstudiante;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public int getIdEstudiante() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiante(int idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "idEstudiante=" + idEstudiante +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
